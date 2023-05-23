e129031c873d454f9cdd2861f0a0163376d1044f
sekkiyah <seth_hinshaw@hotmail.com>
2023-05-22 23:09:45 -0500

    initial commit

---
+++ src/main/java/sekk/backbiz/BackbizApplication.java
@@ -0,0 +1,13 @@
+package sekk.backbiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackbizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackbizApplication.class, args);
+	}
+
+}
